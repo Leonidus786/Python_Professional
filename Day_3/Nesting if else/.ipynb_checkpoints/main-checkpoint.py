43352adfa4cf83ec18c13809f792f191448bf414
Leonidus786 <abhi.upadh@gmail.com>
2024-10-22 22:15:37 +0530

    list

---
+++ Day_3/Nesting if else/.ipynb_checkpoints/main-checkpoint.py
@@ -0,0 +1,23 @@
+# maths_score = int(input("Enter your maths marks 0-100: "))
+# english_score = int(input("Enter your english marks 0-100: "))
+#
+# if maths_score >= 90:
+#     if english_score >= 90:
+#         print("You're good at everything")
+#     else:
+#         print("You're good at maths")
+# if english_score >= 90:
+#     print("You're good at english")
+
+
+
+days = int(input("Enter the day no.: "))
+
+if days == 1:
+     print("Sunday")
+elif days == 2:
+    print("Monday")
+elif days == 3:
+    print("Tuesday")
+
+
